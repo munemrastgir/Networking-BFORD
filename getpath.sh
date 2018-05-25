fc5114f526b313e43218acff0f0ba998e8ba7adb
Glyne Gittens <jgit121@gmail.com>
2018-05-25 16:03:09 -0400

    Fixed bugs with quote situation

--- getpath.sh
+++ getpath.sh
@@ -18,32 +18,32 @@ NODE=$(echo "$NODE" | tr '[:upper:]' '[:lower:]')
 
 if [ "$NODE" == "bellman-ford" ];
 then
-	$(mtr 127.0.0.1 --report --no-dns)
+	mtr "127.0.0.1" --report --no-dns
 fi
 
 if [ "$NODE" == "lovelace" ];
 then
-	$(mtr 10.10.4.1 --report --no-dns)
+	mtr "10.10.4.1" --report --no-dns
 fi
 
 if [ "$NODE" == "knuth" ];
 then
-	$(mtr 10.10.8.2 --report --no-dns)
+	mtr "10.10.8.2" --report --no-dns
 fi
 
 if [ "$NODE" == "hopper" ];
 then
-	$(mtr 10.10.2.1 --report --no-dns)
+	mtr "10.10.2.1" --report --no-dns
 fi
 
 if [ "$NODE" == "cerf" ];
 then
-	$(mtr 10.10.1.1 --report --no-dns)
+	mtr "10.10.1.1" --report --no-dns
 fi
 
 if [ "$NODE" == "baran" ];
 then
-	$(mtr 10.10.5.2 --report --no-dns)
+	mtr "10.10.5.2" --report --no-dns
 fi
 
 exit 0
